3b23ddf823b421f126d00c85fcd069d01e5d40a2
bruno <bruno.rsilva2004@gmail.com>
2026-01-23 18:14:10 -0300

    adiciona a primeira versão da página de consulta e cadastro de produtos

--- resources/js/Pages/Products/Index.jsx
+++ resources/js/Pages/Products/Index.jsx
@@ -3,8 +3,24 @@ import { Head } from '@inertiajs/react';
 
 export default function ProductsIndex() {
     return (
-        <AuthenticatedLayout>
+        <AuthenticatedLayout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                    Produtos
+                </h2>
+            }
+        >
             <Head title='Produtos'></Head>
+
+            <div className='py-12'>
+                <div className='mx-auto max-w-7xl sm:px-6 lg:px-8'>
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            Página de Produtos!
+                        </div>
+                    </div>
+                </div>
+            </div>
         </AuthenticatedLayout>
     )
 }
